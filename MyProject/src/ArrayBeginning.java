cda85e9e77673d995b8b9d17004e324df113ca96
Isaack346 <iam.isaack346@gmail.com>
2022-05-29 09:49:43 -0400

    In this project i used Array to do seating chart.

---
+++ MyProject/src/ArrayBeginning.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ArrayBeginning {
+	public static void main(String[]args) {
+		System.out.println("Goodmorning Class today you will take your 5th and final exam.");
+		System.out.println("It's time to take your seats.\n");
+		String[] names = {"Harry" , "Neville", "Ron" , "Hermione" , "Seamus" };
+		int [] seats = {0 , 1, 2, 3 , 4 };
+		System.out.println(names[0] + " , you will take seat " + seats[0]);
+		System.out.println(names[1] + " , you will take seat " + seats[1]);
+	    System.out.println(names[2] + " , you will take seat " + seats[2]);
+		System.out.println(names[3] + " , you will take seat " + seats[3]);
+		System.out.println(names[4] + " , you will take seat " + seats[4]);
+		
+		
+	}
+
+}
