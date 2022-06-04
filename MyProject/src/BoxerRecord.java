d9cffab30feae5e0d726530298647d6478e91bd3
Isaack346 <iam.isaack346@gmail.com>
2022-06-04 13:38:35 -0400

    In this Program it prints the record of a boxer then the name of a boxer.

---
+++ MyProject/src/BoxerRecord.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class BoxerRecord {
+    public static void main(String[]args) {
+        int[]record = { 1 , 2, 3 , 4, 5};
+        String[]names = {"Tank Davis" , "Floyd Maywheather" , "Mike Tyson" , " Mohammed Ali"};
+        System.out.println("With a professional record of " + record[4] + " wins and " + record[1] + " losses");
+        System.out.println("He is the pride of oracle: " + names[0]);
+        /**
+         * Here I created two different arrays one that is an integer with numbers 1-5
+         * Then I created a String array with different boxer names
+         * Last I created two print statements one with the boxers record
+         * the next with the namer of the boxer
+         */
+
+    }
+
+}
