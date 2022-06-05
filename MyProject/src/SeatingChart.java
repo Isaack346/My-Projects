e02820c2fa356b12558af9c4687d11e933624473
Isaack346 <iam.isaack346@gmail.com>
2022-06-04 20:22:07 -0400

    In this project I created a seating chart

---
+++ MyProject/src/SeatingChart.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class SeatingChart {
+public static void main(String[]args) {
+	String[]names = {"Melfoy" , "Crabbe" , "Goyle", "Pansy" , "Dean" };
+	for(int i = 0; i < names.length; i++) {
+		System.out.println(names[i] + " you will take seat " + i);
+		/**
+		 * Above I created an array 
+		 * Then I created a for loop staring form 0
+		 * Last i created a print statement where the print statement says 
+		 * that each person will take a seat in a Specific number
+		 */
+	}
+}
+}
